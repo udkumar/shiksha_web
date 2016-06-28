51f44afd0c175ff76052b5c3d5c8b15e255bff5c
uday <udayonrails@hotmail.com>
2016-06-28 14:17:21 +0530

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,37 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery-2.1.3.min
+//= require jquery_ujs
+//= require jquery.viewport
+//= require jquery.easing.1.3
+//= require jquery.simpleplaceholder
+//= require jquery.fitvids
+//= require animate
+//= require hoverIntent
+//= require superfish
+//= require jquery.themepunch.tools.min
+//= require jquery.themepunch.revolution.min
+//= require jquery.bxslider.min
+//= require jquery.magnific-popup.min
+//= require imagesloaded.pkgd.min
+//= require isotope.pkgd.min
+//= require jquery.parallax.min
+//= require jquery.easypiechart.min
+//= require jquery.mb.YTPlayer
+//= require jquery.easytabs.min
+//= require jquery.validate.min
+//= require jquery.form.min
+//= require jquery.gmap.min
+//= require twitterfetcher
+//= require main
+//= require jquery_cookie
